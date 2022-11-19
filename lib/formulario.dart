3351edacd0e19d9bf009950cdf33ff4806beecb4
ramon <ramon.basilio@hotmail.com>
2022-11-18 23:41:11 -0300

    Criação da tela basica para inserção de dados de cada contato. 18/11/2022.

--- lib/formulario.dart
+++ lib/formulario.dart
@@ -1,4 +1,3 @@
-import 'package:agenda_contatos/classes-tarefas/campoContato.dart';
 import 'package:flutter/material.dart';
 
 class formContato extends StatefulWidget {
@@ -7,6 +6,17 @@ class formContato extends StatefulWidget {
   @override
   State<formContato> createState() => _formContatoState();
 }
+
+const double padding01 = 15.0;
+final dropOpcoes = [
+  'Familia',
+  'Trabalho',
+  'Faculdade',
+  'Igreja',
+  'Outros',
+];
+
+String dropdownValue = dropOpcoes.first;
 
 class _formContatoState extends State<formContato> {
   TextEditingController nomeContato = TextEditingController();
@@ -17,109 +27,210 @@ class _formContatoState extends State<formContato> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Criar um novo contato'),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 50.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.lightGreen,
-                foregroundColor: Colors.black,
-                side: const BorderSide(color: Colors.black),
+    return Form(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Criar um novo contato'),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 50.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.lightGreen,
+                  foregroundColor: Colors.black,
+                  side: const BorderSide(color: Colors.black),
+                ),
+                onPressed: () {
+                  print('Apertou o botao');
+                },
+                child: Text('Salvar'),
               ),
-              onPressed: () {
-                print('Apertou o botao');
-              },
-              child: Text('Salvar'),
             ),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Column(
-                    children: [
-                      ClipRRect(
-                        child: Image.asset('img/add_foto.png'),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Column(
+                      children: [
+                        ClipRRect(
+                          child: Image.asset('img/add_foto.png'),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 15),
+                          child: Text('Adicionar foto'),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(padding01),
+                      child: DropdownButtonFormField(
+                        decoration: const InputDecoration(
+                              labelText: 'Tipo de contato',
+                              contentPadding:
+                              EdgeInsets.symmetric(horizontal: 20),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide:
+                                  BorderSide(width: 1, color: Colors.black))),
+                        hint: const Text('Tipo de contato'),
+                        value: dropdownValue,
+                        onChanged: (String? value) {
+                          // This is called when the user selects an item.
+                          setState(
+                            () {
+                              dropdownValue = value!;
+                            },
+                          );
+                        },
+                        items: dropOpcoes
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
                       ),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 15),
-                        child: Text('Adicionar foto'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(padding01),
+                      child: TextFormField(
+                        controller: nomeContato,
+                        decoration: const InputDecoration(
+                            labelText: 'Nome',
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black))),
                       ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: nomeContato,
-                      decoration: const InputDecoration(
-                          labelText: 'Nome',
-                          contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 1, color: Colors.black))),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: sobrenomeContato,
-                      decoration: const InputDecoration(
-                          labelText: 'Sobrenome',
-                          contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 1, color: Colors.black))),
+                    Padding(
+                      padding: const EdgeInsets.all(padding01),
+                      child: TextFormField(
+                        controller: sobrenomeContato,
+                        decoration: const InputDecoration(
+                            labelText: 'Sobrenome',
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black))),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: celularContato,
-                      decoration: const InputDecoration(
-                          labelText: 'Celular',
-                          contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 1, color: Colors.black))),
+                    Padding(
+                      padding: const EdgeInsets.all(padding01),
+                      child: TextFormField(
+                        controller: celularContato,
+                        decoration: const InputDecoration(
+                            labelText: 'Celular',
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black))),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: emailContato,
-                      decoration: const InputDecoration(
-                          labelText: 'E-mail',
-                          contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 1, color: Colors.black))),
+                    Padding(
+                      padding: const EdgeInsets.all(padding01),
+                      child: TextFormField(
+                        controller: emailContato,
+                        decoration: const InputDecoration(
+                            labelText: 'E-mail',
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black))),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: aniversarioContato,
-                      decoration: const InputDecoration(
-                          labelText: 'Aniversario',
-                          contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 1, color: Colors.black))),
+                    Padding(
+                      padding: const EdgeInsets.all(padding01),
+                      child: TextFormField(
+                        controller: aniversarioContato,
+                        decoration: const InputDecoration(
+                            labelText: 'Aniversario',
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black))),
+                      ),
                     ),
-                  ),
-               ],
-              ),
-            ],
+                    Padding(
+                      padding: const EdgeInsets.all(padding01),
+                      child: TextFormField(
+                        controller: aniversarioContato,
+                        decoration: const InputDecoration(
+                            labelText: 'Rua',
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black))),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(padding01),
+                      child: TextFormField(
+                        controller: aniversarioContato,
+                        decoration: const InputDecoration(
+                            labelText: 'Numero',
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black))),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(padding01),
+                      child: TextFormField(
+                        controller: aniversarioContato,
+                        decoration: const InputDecoration(
+                            labelText: 'Bairro',
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black))),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(padding01),
+                      child: TextFormField(
+                        controller: aniversarioContato,
+                        decoration: const InputDecoration(
+                            labelText: 'Cidade',
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black))),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(padding01),
+                      child: TextFormField(
+                        controller: aniversarioContato,
+                        decoration: const InputDecoration(
+                            labelText: 'CEP',
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black))),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
